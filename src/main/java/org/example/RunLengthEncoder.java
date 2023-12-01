f894fe9b294468e9c930a45a1a92df134ee8f577
jwarren-scottlogic <jwarren@scottlogic.com>
2023-12-01 13:23:02 +0000

    SingleCharacterSearch

--- src/main/java/org/example/RunLengthEncoder.java
+++ src/main/java/org/example/RunLengthEncoder.java
@@ -6,6 +6,10 @@ import java.util.Objects;
 
 public class RunLengthEncoder {
     public static String runLengthEncoder(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            return "";
+        }
+
         String outputString = "";
         List<String> inputStringList = new ArrayList<>(List.of(inputString.split("")));
         for (int j = 0; j < inputStringList.size();) {
@@ -14,10 +18,7 @@ public class RunLengthEncoder {
 
             for (int i = j; i < inputStringList.size(); i++) {
                 boolean currentLetterEqualsPrevious = Objects.equals(inputStringList.get(i), letter);
-
-                if (currentLetterEqualsPrevious) {
-                    counter++;
-                }
+                if (currentLetterEqualsPrevious) counter++;
 
                 boolean isiLastCharacter = (i == inputStringList.size()-1);
                 if (!currentLetterEqualsPrevious || (isiLastCharacter)){
@@ -39,7 +40,30 @@ public class RunLengthEncoder {
         return outputString;
     }
 
+
+//chatGPT version: (much cleaner)
+    public static String runLengthEncode(String input) {
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder encoded = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i <= input.length(); i++) {
+            if (i == input.length() || input.charAt(i) != input.charAt(i - 1)) {
+                encoded.append(input.charAt(i - 1));
+                encoded.append(count);
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        return encoded.toString();
+    }
+
         public static void main(String[] args){
-            System.out.println(runLengthEncoder("abggsss"));
+            System.out.println(runLengthEncoder(""));
         }
     }
---
+++ src/main/java/org/example/SingleCharacterSearch.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class SingleCharacterSearch {
+
+    public static char findCharacter(String inputString) throws Exception{
+        while (inputString.length()>0){
+            char letter = inputString.charAt(0);
+            int nextOccurrence = inputString.indexOf(letter, 1);
+            if (nextOccurrence == -1) {
+                return inputString.charAt(0);
+            }
+            inputString=inputString.replaceAll(String.valueOf(letter), "");
+        }
+        throw new Exception("No single character found.");
+    }
+
+
+    public static void main(String[] args) {
+        String input = "";
+        try {
+            char singleCharacter = findCharacter(input);
+            System.out.println("single character: " + singleCharacter);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
